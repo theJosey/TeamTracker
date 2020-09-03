b4e2048d16087e0083af46746a853154f4e6efa8
Kevin Josey <kevinjosey13@gmail.com>
2020-09-02 20:18:51 -0700

    First night of notes

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
+<!-- For now this can stay as one page 
+    Maybe each sport gets it's own page? 
+    Or just have a dropdown to select each sport? 
+    can make fancier css with different pages?
+    Starting with Baseball  
+-->
 <template>
     <div>
+     
     </div>
 </template>
